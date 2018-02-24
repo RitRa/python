82de07c942bd53b39c62f1d88f3ab241cbe60e28
Rita Raher <ritaraher@gmail.com>
2018-02-24 10:45:30 +0000

    project euler 3

--- projecteuler_3.py
+++ projecteuler_3.py
@@ -4,4 +4,40 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 # https://projecteuler.net/problem=3
+# prime factors of a positive integer are the prime numbers that divide that integer exactly
+
+# using SymPy
+# SymPy is a Python library for symbolic mathematics
+# Ref: http://docs.sympy.org/latest/modules/ntheory.html
+
+from sympy import primefactors
+
+# prime factors for 13195
+print(primefactors(13195))
+
+# prime factors for 600851475143
+print(primefactors(600851475143))
+
+
+def prime_factors(n):
+    #create an empty list to hold the factors
+    factors = []
+    # start with 2
+    i = 2
+    # check the number is greater than 1 to continue
+    while n > 1:
+        # while the number divided i with no remainder
+        while n % i == 0:
+            # add the number to the list
+            factors.append(i)
+            # number = number / factor
+            n = n / i  
+        #increase i 
+        i += 1
+    #return list
+    return factors
+
+print(prime_factors(13195))
+
+
 
